60ee50304dc653f05028266123bdeee260d731cc
douglaswenzel <douglas.wenzel@hotmail.com>
2025-03-31 19:25:36 -0300

    App.ts configurado para receber as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './screens/Login/login';
+import theme from './theme/theme';
+import AppRouter from './routes/AppRouter';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2',
-    },
-    background: {
-      default: '#f5f5f5',
-      paper: '#ffffff',
-    },
-  },
-});
-
-const App = () => {
+function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<div>Dashboard (em construção)</div>} />
-        </Routes>
-      </Router>
+      <AppRouter />
     </ThemeProvider>
   );
-};
+}
 
 export default App;

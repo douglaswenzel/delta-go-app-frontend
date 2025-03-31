e4f81d928be4610740f9e1c6378f8dedbd822c84
douglaswenzel <douglas.wenzel@hotmail.com>
2025-03-31 19:54:43 -0300

    UserRegisterScreen set

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import Loading from '../components/Loading/Loading';
 import DashboardScreen from '../screens/Dashboard/Dashboard';
+import UserRegisterScreen from '../screens/UserRegister/UserRegister';
 
 const Home = lazy(() => import('../screens/Home/HomeScreen'));
 const Login = lazy(() => import('../screens/Login/LoginScreen'));
@@ -17,6 +18,7 @@ const AppRouter = () => {
           <Route path="/recuperar-senha" element={<PasswordRecovery />} />  
           <Route path="*" element={<Navigate to="/" replace />} />
           <Route path="/dashboard" element={<DashboardScreen />} />
+          <Route path="/cadastro-usuario" element={<UserRegisterScreen />} />        
         </Routes>
       </Suspense>
     </BrowserRouter>

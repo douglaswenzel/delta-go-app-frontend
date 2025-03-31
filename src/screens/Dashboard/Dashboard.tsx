a2766391fb631d156f5c57abdd9e541123e9dc89
douglaswenzel <douglas.wenzel@hotmail.com>
2025-03-31 19:36:22 -0300

    DashboardScreen set

---
+++ src/screens/Dashboard/Dashboard.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { 
+  DashboardContainer,
+  Title,
+  ButtonsGrid,
+  DashboardButton,
+  ButtonIcon,
+  ButtonText
+} from './styles';
+import { 
+  PersonAdd as PersonAddIcon,
+  SwapHoriz as MovimentationsIcon,
+  Assessment as ReportsIcon,
+  People as UsersIcon
+} from '@mui/icons-material';
+
+const DashboardScreen = () => {
+  return (
+    <DashboardContainer>
+      <Title variant="h4">Bem-vindo ao Painel de Controle</Title>
+      
+      <ButtonsGrid>
+        {/* Botão Cadastro de Pessoas */}
+        <DashboardButton>
+          <ButtonIcon>
+            <PersonAddIcon fontSize="inherit" />
+          </ButtonIcon>
+          <ButtonText>Cadastro de Pessoas</ButtonText>
+        </DashboardButton>
+
+        {/* Botão Movimentações */}
+        <DashboardButton>
+          <ButtonIcon>
+            <MovimentationsIcon fontSize="inherit" />
+          </ButtonIcon>
+          <ButtonText>Movimentações</ButtonText>
+        </DashboardButton>
+
+        {/* Botão Relatórios */}
+        <DashboardButton>
+          <ButtonIcon>
+            <ReportsIcon fontSize="inherit" />
+          </ButtonIcon>
+          <ButtonText>Relatórios</ButtonText>
+        </DashboardButton>
+
+        {/* Botão Listagem de Usuários */}
+        <DashboardButton>
+          <ButtonIcon>
+            <UsersIcon fontSize="inherit" />
+          </ButtonIcon>
+          <ButtonText>Listagem de Usuários</ButtonText>
+        </DashboardButton>
+      </ButtonsGrid>
+    </DashboardContainer>
+  );
+};
+
+export default DashboardScreen;

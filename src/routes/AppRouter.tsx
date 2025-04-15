e6dfd51c7895f860872d9fdfc8ba5977f793f801
douglaswenzel <douglas.wenzel@hotmail.com>
2025-04-15 15:44:56 -0300

    v.2.001.001 - /movimentacoes | Rota definida

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,6 +3,7 @@ import { lazy, Suspense } from 'react';
 import Loading from '../components/Loading/Loading';
 import DashboardScreen from '../screens/Dashboard/Dashboard';
 import UserRegisterScreen from '../screens/UserRegister/UserRegister';
+import MovimentacoesScreen from '../screens/Movimentacoes/MovimentacoesScreen';
 
 const Home = lazy(() => import('../screens/Home/HomeScreen'));
 const Login = lazy(() => import('../screens/Login/LoginScreen'));
@@ -18,7 +19,8 @@ const AppRouter = () => {
           <Route path="/recuperar-senha" element={<PasswordRecovery />} />  
           <Route path="*" element={<Navigate to="/" replace />} />
           <Route path="/dashboard" element={<DashboardScreen />} />
-          <Route path="/cadastro-usuario" element={<UserRegisterScreen />} />        
+          <Route path="/cadastro-usuario" element={<UserRegisterScreen />} />
+          <Route path="/movimentacoes" element={<MovimentacoesScreen />} /> 
         </Routes>
       </Suspense>
     </BrowserRouter>
